6d73758f205f9fc4eb20ab22d102695116e8e6cd
ckaltenbach904 <catharina904@outlook.com>
2019-04-04 00:03:57 +0200

    Update program.rb

--- program.rb
+++ program.rb
@@ -4,17 +4,18 @@ p "hello"
 my_name = "Catharina"
 greeting("Catharina")
 
-def greeting(name)
-    p "hello" + "" + name
-end
+def greeting(name)# here we define a method called "greeting" and an argument "name" within a parentheses.
+    p "hello " + "" + name
+end #closing method
 
 greeting(my_name)
 
-def greeting
+
+def greeting#here we define a method called "greeting"
     puts "Please enter your name:"
-    name = gets.chomp
+    name = gets.chomp#here is where the inside the method
     puts "Hello" + " " + name
-  end
+  end#closing method
 
 greeting
 
