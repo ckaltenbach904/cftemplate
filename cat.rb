cbe66c4092bd32e5f3fd61dcdd9d9defdd43ad89
ckaltenbach904 <catharina904@outlook.com>
2019-04-08 17:45:02 +0200

    message

--- cat.rb
+++ cat.rb
@@ -29,7 +29,7 @@ puts kitty.inspect
 puts "What class does our new cat belong to?"
 puts kitty.class
 puts "Is our new cat an object?"
-puts kitty.is_a(Object)
+puts kitty.is_a?(Object)
 puts "What color is our cat?"
 puts kitty.color
 puts "Let's give our new cat a name"
